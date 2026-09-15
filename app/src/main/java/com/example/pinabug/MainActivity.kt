c617867906617805c72921705255011f7b15a6be
Kylah Irvine <st10435066@rcconnect.edu.za>
2026-09-15 10:24:02 +0200

    Untested map implementation
    Signed-off-by: Kylah Irvine <ST10435066@rcconnect.edu.za>

--- app/src/main/java/com/example/pinabug/MainActivity.kt
+++ app/src/main/java/com/example/pinabug/MainActivity.kt
@@ -9,6 +9,12 @@ import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import org.osmdroid.config.Configuration
+import org.osmdroid.views.MapView
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+
 
 class MainActivity : ComponentActivity() {
     var auth: FirebaseAuth? = null
@@ -16,15 +22,24 @@ class MainActivity : ComponentActivity() {
     var textView: TextView? = null
     var user: FirebaseUser? = null
 
+    private lateinit var map: MapView
+
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Configuration.getInstance().load(this, getSharedPreferences("osmdroid", MODE_PRIVATE))
         setContentView(R.layout.activity_main)
+
 
         auth = FirebaseAuth.getInstance()
         button = findViewById<Button>(R.id.logout)
-        textView = findViewById<TextView>(R.id.user_details)
+//        textView = findViewById<TextView>(R.id.user_details)
         user = auth!!.getCurrentUser()
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
 
         if (user == null) {
             val intent = Intent(getApplicationContext(), Login::class.java)
@@ -41,5 +56,15 @@ class MainActivity : ComponentActivity() {
                 finish()
             }
         })
+
+        val controller = map.controller
+        controller.setZoom(15.0)
+        controller.setCenter(GeoPoint(-33.9249, 18.4241)) // Cape Town
+
+        // Add a test marker
+        val marker = Marker(map)
+        marker.position = GeoPoint(-33.9249, 18.4241)
+        marker.title = "Bug spotted!"
+        map.overlays.add(marker)
     }
 }
